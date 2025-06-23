373f96ee86549fe0b6385c5cc590949ef023b9ec
oms287568 <oms287568@gmail.com>
2025-06-23 21:38:24 +0530

    Fix generate.js

--- generate.js
+++ generate.js
@@ -9,6 +9,7 @@ export default async function handler(req, res) {
           "Content-Type": "application/json",
           "Authorization": "Bearer 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"
 
+
         },
         body: JSON.stringify({
           inputs: prompt
