e3f7bb317d9a0e10046c51846237355a24eae207
oms287568 <oms287568@gmail.com>
2025-06-24 08:44:08 +0530

    Update generate.js

--- generate.js
+++ generate.js
@@ -5,7 +5,8 @@ export default async function handler(req, res) {
     const response = await fetch("https://api-inference.huggingface.co/models/stabilityai/stable-diffusion-2", {
   method: "POST",
   headers: {
-    "Authorization": `Bearer ${process.env.HUGGINGFACE_API_KEY}`,
+    "Authorization": `Bearer ${process.env.API_TOKEN}`,
+
     "Content-Type": "application/json"
   },
   body: JSON.stringify({
